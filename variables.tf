f7d2428a3858d45f5d6487f9de5dfcebb93a98f0
Callender, Fitzgerald <Fitzgerald.Callender@Markel.com>
2020-07-20 18:54:16 +0000

    Added file variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "client_id" {}
+variable "client_secret" {}
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "env_name" {}
+
+/*
+variable "ProjectCode" {
+    type      = string
+    default   = "50220"
+}
+*/
